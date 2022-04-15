8539b51e0b56a73e86a5f48d79b7ba2f3dfe8b79
Reham Alswaisi <ri7am.3.ri7am@gmail.com>
2022-04-15 13:02:14 +0300

    rehamalswaisi

---
+++ lib/todo_app/widgets/task_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:state_managment/todo_app/models/task_model.dart';
+
+class TaskWidget extends StatelessWidget {
+  Function fun;
+  Task task;
+  Function delete;
+  TaskWidget(this.task, this.fun, this.delete);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+          color: task.isCompleted ? Colors.green : Colors.black45,
+          borderRadius: BorderRadius.circular(15)),
+      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+      child: CheckboxListTile(
+        secondary: IconButton(
+          icon: const Icon(Icons.delete,color: Colors.red,),
+          onPressed: (){
+            delete(task);
+          },
+        ),
+        title: Text(task.title),
+        value: task.isCompleted,
+        onChanged: (v) {
+          fun(task);
+        },
+      ),
+    );
+  }
+}
